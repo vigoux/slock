a9187a11502d51103f34a722766202dc4627e448
Thomas Vigouroux <tomvig38@gmail.com>
2020-05-12 11:06:20 +0200

    feat: apply dpms patch

--- config.def.h
+++ config.def.h
@@ -4,9 +4,12 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#6272a4",   /* during input */
+	[FAILED] = "#ff5555",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
